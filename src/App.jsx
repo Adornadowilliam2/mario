98f1e020a117789d586399d27792a970cc77443d
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-07-17 22:45:21 +0800

    add some design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import logo from "./media/logo.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [appear, setAppear] = useState(false);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setAppear(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <span>
+          <img src={logo} alt="Smile" />
+          <h1>My Website</h1>
+        </span>
+      </nav>
+      <main>
+        {appear ? (
+          <div>
+            <h1>
+              Welcome {fname} {lname}
+            </h1>
+          </div>
+        ) : (
+          <form onSubmit={onSubmit}>
+            <div>
+              <label htmlFor="fname">First Name:</label>
+              <input
+                type="text"
+                id="fname"
+                value={fname}
+                placeholder="First Name"
+                onChange={(e) => setFname(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="lname">Last Name:</label>
+              <input
+                type="text"
+                id="lname"
+                value={lname}
+                placeholder="Last Name"
+                onChange={(e) => setLname(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

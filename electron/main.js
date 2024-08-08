b7db6ea252696966c36418cbf1a5148a476c8489
Adornadowilliam2 <145187649+Adornadowilliam2@users.noreply.github.com>
2024-08-08 15:33:40 +0800

    create a new branch for electron

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000"); // URL where Vite serves your React app
+  // For production, use:
+  // mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
